9842715aab5fe790f11f06d3ea6ce5367325b160
csubira <cillas@cartodb.com>
2018-03-19 09:51:39 +0100

    Add ansible to vagrantfile

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# status-page
+# Status Page
+
+## About
+
+This is a status page that reflects the health of the platform and that shows information about a ongoing outage.
+
+The three different status reflected are:
+ - Uptime (the platform is healthy)
+ - Partial degradation (the platform is having some issues)
+ - Downtime (the platform is down)
+
+This status page has two different modes: automatic and manual:
+
+- Automatic: The automatic mode feeds from existing metrics that are generated every minute.
+- Manual: The manual mode allows a user to update the status of the platform with a manual message through a interactive web interface where the user can write the message and all the metadata related with the message.
+         
+## Installation
+
+- Download and install [Vagrant](https://www.vagrantup.com/) for your platform.
+- Download and install [VirtualBox](https://www.virtualbox.org/) for your platform.
+
+- Clone the repository 
+	`git clone git@github.com:csubira/status-page.git`
+	`cd status-page`
+- Run `vagrant up`
+
+- Open in your browser 
+	`http://0.0.0.0:5000/status-page`
+
+## Play with it
+
+- In Automatic mode you can see the latest updates from the service status
+- If you click to change mode to Manual, an additional button appears nes to last status. 
+A new status can be manually added filling the form.
+Note: While manual mode is active, there is not any automatic update registered.
+Once the automatic mode is restored, the regular of service status feed is resumed.
+
+_All status are stored in a log in order to be available in the future_ 
--- app.py
+++ app.py
@@ -2,45 +2,34 @@
 from flask import Flask, g, session, jsonify, render_template, request, redirect, url_for, json, flash
 from forms import StatusForm
 from datetime import datetime
-import logging
 
 
 app = Flask(__name__)
+
+# Config
 app.config['SECRET_KEY'] = 'you-will-never-guess'
 app.config['AUTO_MODE'] = True
 MODES = dict([(True, "Automatic"), (False, "Manual")])
-
-# logger = logging.getLogger('myapp')
-# hdlr = logging.FileHandler('myapp.log')
-# logging.basicConfig(filename='example.log')
-# hdlr.setFormatter(formatter)
-# app.config['WTF_CSRF_ENABLED'] = False
-statusList=[
+STATUS_TEXT = {0: "The platform is down", 1: "The platform is healty", 2: "The platform is having some issues"}
+STATUS_LIST=[
  {
  'status': 1,
- 'message':'Working',
- 'date':'Today',
- 'auto_mode': True
- },
- {
- 'status': 0,
- 'message':'Not Working',
- 'date':'Today',
- 'auto_mode': True
- },{
- 'status': 2,
- 'message':'Psa',
- 'date':'Today',
- 'auto_mode': True
- },{
- 'status': 1,
- 'message':'Not Working',
- 'date':'Today',
+ 'message': STATUS_TEXT[1],
+ 'date': datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
  'auto_mode': True
  }
 ]
 
+def save_status(status):
+    file = open('status.log', 'a')
+    status_log = "%s %s %s \n" % (MODES[status['auto_mode']], status['date'], status['message'])
+    file.write(status_log)
+    file.close()
 
+save_status(STATUS_LIST[0])
+
+
+# Routes
 @app.route('/change_mode', methods=['POST'])
 def change_mode():
     app.config['AUTO_MODE'] = not app.config['AUTO_MODE']
@@ -50,42 +39,24 @@ def change_mode():
 
 @app.route('/status-page',methods=['GET'])
 def get_all_status():
-    # logging.debug('This message should go to the log file')
-    # logging.info('So should this')
-    # logging.warning('And this, too')
-    # logger = logging.getLogger('INFO')
-    # get_mode()
-    # logger = logging.getLogger('myapp')
-    # hdlr = logging.FileHandler('myapp.log')
-    # formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-    # hdlr.setFormatter(formatter)
-    # logger.addHandler(hdlr) 
-    # logger.setLevel(logging.WARNING)
-    # logger.error('We have a problem')
-    # logger.info('While this is just chatty')
     g._mode = {'auto': app.config['AUTO_MODE'], 'text': MODES[app.config['AUTO_MODE']] }
-    return render_template('main.html', status_list=reversed(statusList))
+    return render_template('main.html', status_list=reversed(STATUS_LIST))
 
 @app.route('/status-page/update', methods=['POST'])
 def update():
     if app.config['AUTO_MODE']:
         r = json.loads(request.data)
+        status_num = r['status']
 
-        dat = {
+        current_status = {
         'status': r['status'],
-        'message':r['message'],
+        'message': STATUS_TEXT[status_num],
         'date': r['date'],
         'auto_mode': True
         }
 
-        statusList.append(dat)
-        # return jsonify({'response':'Success'})
-        # file = open("testfile.txt","w") 
-        # file.write(''.join(statusList))
-        # file.close() 
-
-        # print statusList
-        # return render_template('main.html', status_list=statusList)
+        STATUS_LIST.append(current_status)
+        save_status(current_status)
     return jsonify({'response':'Success'})
 
 
@@ -94,73 +65,21 @@ def manual_update():
     if app.config['AUTO_MODE']:
         flash('You cannot update an status in Automatic Mode!')
         return redirect(url_for('get_all_status'))
-    form = StatusForm()
-    print 'hello'    
-    print form.errors
 
+    form = StatusForm()
     if form.is_submitted():
-        dat = {
+        new_status = {
             'status': int(form.status.data),
             'message': str(form.message.data),
-            'date': str(datetime.now()),
+            'date': datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
             'auto_mode': False
         }
-        print dat
-        statusList.append(dat)
-
+        STATUS_LIST.append(new_status)
+        save_status(new_status)
         return redirect(url_for('get_all_status'))
-
-    # if form.validate():
-    #     print "valid"
-
-    # if form.validate_on_submit():
- 
-    #     # Check the password and log the user in
-    #     # [...]
-    #     print 'estamos aqui'
-    #     print form.status
  
     return render_template('manual_update.html', form=form)
-# @app.route('/')
-# def index():
-#     return "Hello, World!"
 
-# @app.route('/empdb/employee',methods=['GET'])
-# def getAllEmp():
-#     # return jsonify({'emps':empDB})
-#     return render_template('main.html', status_list=empDB)
-
-# @app.route('/empdb/employee/<empId>',methods=['GET'])
-# def getEmp(empId):
-#     usr = [ emp for emp in empDB if (emp['id'] == empId) ] 
-#     return jsonify({'emp':usr})
-
-# @app.route('/empdb/employee/<empId>',methods=['PUT'])
-# def updateEmp(empId): 
-#     em = [ emp for emp in empDB if (emp['id'] == empId) ] 
-#     if 'name' in request.json : 
-#         em[0]['name'] = request.json['name'] 
-#     if 'title' in request.json:
-#         em[0]['title'] = request.json['title'] 
-#     return jsonify({'emp':em[0]})
-
-# @app.route('/empdb/employee',methods=['POST'])
-# def createEmp(): 
-#     dat = {
-#     'id':request.json['id'],
-#     'name':request.json['name'],
-#     'title':request.json['title']
-#     }
-#     empDB.append(dat)
-#     return jsonify(dat)
-
-# @app.route('/empdb/employee/<empId>',methods=['DELETE'])
-# def deleteEmp(empId): 
-#     em = [ emp for emp in empDB if (emp['id'] == empId) ] 
-#     if len(em) == 0:
-#         abort(404) 
-#     empDB.remove(em[0])
-#     return jsonify({'response':'Success'})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
